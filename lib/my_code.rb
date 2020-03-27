707e63d96c2fbc5b91f0afdbf27442cb049c9918
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-27 23:27:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+
+def map(source_array)
+element_index =  0
+new_array = []
+while element_index < source_array.length do
+  new_array << yield(source_array[element_index])
+  element_index += 1
+end
+new_array
+end
+
+def reduce(source_array, starting_value = 0)
+  element_index = 0
+  value = starting_value
+  while element_index < source_array.length do
+    new_value = yield(value, source_array[element_index])
+    value = new_value
+    element_index += 1
+  end 
+  if new_value == 0
+    new_value = true
+    else 
+      new_value
+    end
+end
+
+
+  
+    
+
